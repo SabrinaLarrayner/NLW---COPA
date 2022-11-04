b6873ea0012368409120db7fe99e508f1f32ad74
Sabrina <sabrinalarrayner2015@outlook.com>
2022-11-04 00:34:28 -0400

    Quarto commit

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-function creatCard() {
+function creatGame(player1, hour, player2) {
+  return `
+  <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+        <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+  </li>
+      `
+}
+function creatCard(date, day, games) {
   return `
   <div class="Cards">
-          <h2>24/11 <span>Quinta</span></h2>
+          <h2>${date}<span>${day}</span></h2>
           <ul>
-            <li>
-              <img src="./assets/icon-brazil.svg" alt="Bandeira do Brasil" />
-              <strong>16:00</strong>
-              <img src="./assets/icon-servia.svg" alt="Bandeira da Sérvia" />
-            </li>
+           ${games}
           </ul>
         </div>
     `
@@ -17,9 +22,14 @@ document.querySelector("#app").innerHTML = `
   <header>
         <img src="./assets/logo.svg" alt="Logo da NLW" />
       </header>
-      <main class="card">
-        ${creatCard()}
-        ${creatCard()}
-        ${creatCard()}
+      <main id="card">
+        ${creatCard(
+          "24/11",
+          "Quinta",
+          creatGame("brazil", "16:00", "servia") +
+            creatGame("india", "15:45", "britis")
+        )}
+        ${creatCard("28/11", "Segunda", creatGame("brazil", "13:00", "swit"))}
+        ${creatCard("12/12", "Sexta", creatGame("camaron", "16:00", "brazil"))}
       </main>
 `
